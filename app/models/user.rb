eeeaffcc2dc3cb0a5d3508a2c4c999b855793a88
Angel Mora <eduardo.aemc@gmail.com>
2017-04-09 16:09:37 -0500

    config the permission level

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,6 +32,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
+  
+  #Mandar a llamar a permisions level
+  include PermissionsConcern
+
   #Funcion para verificar si !existe el usuario login con facebook => crea un usuario
   def self.from_omniauth(auth)
     where(provider: auth[:provider], uid: auth[:uid] ).first_or_create do |user|
