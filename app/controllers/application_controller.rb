e17c29f2a533a003d2a581e0e3fe9b2fa8fda947
Angel Mora <eduardo.aemc@gmail.com>
2017-04-09 16:53:28 -0500

    config authenticate users in aplication_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  protected
+  def authenticate_chanel!
+  	redirect_to root_path, notice: "No eres un Youtubers" unless user_signed_in? && current_user.is_chanel? 
+  end
+
+  def authenticate_admin!
+  	redirect_to root_path, notice: "No eres un admin" unless user_signed_in? && current_user.is_admin? 
+  end
 end
