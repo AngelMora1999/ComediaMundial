8267be4afeabb8c6da512cfd0821b4ef003f4e4f
Angel Mora <eduardo.aemc@gmail.com>
2017-04-10 19:35:16 -0500

    show user

---
+++ app/controllers/usuarios_controller.rb
@@ -0,0 +1,26 @@
+class UsuariosController < ApplicationController
+	before_action :set_user
+
+	def show
+
+	end
+
+	def update
+		respond_to do |format|
+			if @user.update(user_params)
+				format.html{ redirect_to @user, notice: "Si se guardo todo ok" }
+			else
+				format.html{ redirect_to @user, notice:@user.errors.full_messages }
+			end
+		end
+	end
+
+	private
+	def set_user
+		@user = User.find(params[:id])
+	end
+
+	def user_params
+		params.require(:user).permit(:email,:name,:avatar)
+	end
+end
